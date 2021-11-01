3532b1c1274c9647e1c5359d810480982aba501a
Виктор Коротких <korotkih@overmobile.ru>
2021-11-01 15:52:10 +0700

    demo

--- src/settings.49181.js
+++
@@ -1,1 +0,0 @@
-window._CCSettings={platform:"web-desktop",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:false,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/scene/GameScene.fire",orientation:"",jsList:[],bundleVers:{internal:"01588",main:"43ca2"}};
